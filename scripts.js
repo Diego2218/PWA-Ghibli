6662c4647f414a744c2d7c4c76d2827a38a8912c
Diego Vidal Almazan Orozco <dvalmazan@scio.local>
2018-08-20 15:54:55 -0500

    sacando método cargarDatos

--- scripts.js
+++ scripts.js
@@ -33,28 +33,6 @@ if(navigator.onLine){
         });
     }
 }
-
-cargarDatos(response){
-  response.forEach(movie => {
-
-    const card = document.createElement('div');
-    card.setAttribute('class', 'card');
-
-    const h1 = document.createElement('h1');
-    h1.textContent = movie.title;
-
-    const p = document.createElement('p');
-    movie.description = movie.description.substring(0, 300);
-    p.textContent = `${movie.description}...`;
-
-    container.appendChild(card);
-    card.appendChild(h1);
-    card.appendChild(p);
-  });
-}
-
-
-
   /*if (request.status >= 200 && request.status < 400) {
     data.forEach(movie => {
 
@@ -78,6 +56,25 @@ cargarDatos(response){
     app.appendChild(errorMessage);
   }*/
 
+}
+
+cargarDatos(response){
+  response.forEach(movie => {
+
+    const card = document.createElement('div');
+    card.setAttribute('class', 'card');
+
+    const h1 = document.createElement('h1');
+    h1.textContent = movie.title;
+
+    const p = document.createElement('p');
+    movie.description = movie.description.substring(0, 300);
+    p.textContent = `${movie.description}...`;
+
+    container.appendChild(card);
+    card.appendChild(h1);
+    card.appendChild(p);
+  });
 }
 
 request.send();
