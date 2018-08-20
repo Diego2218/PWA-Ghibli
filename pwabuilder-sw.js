00288f219ed8e1180e7278f5e6aecf61a6d609ae
Diego2218 <gogo_18@outlook.com>
2018-08-20 17:17:39 -0500

    Final version

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -14,13 +14,6 @@ var urlsToCache = [
     ];
 
 //Install stage sets up the cache-array to configure pre-cache content
-/*self.addEventListener('install', function(evt) {
-  console.log('[PWA Builder] The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[PWA Builder] Skip waiting on install');
-    return self.skipWaiting();
-  }));
-});*/
 self.addEventListener('install', function(e) {
   console.log('[ServiceWorker] Installing sw');
   e.waitUntil(
@@ -33,10 +26,6 @@ self.addEventListener('install', function(e) {
 
 
 //allow sw to control of current page
-/*self.addEventListener('activate', function(event) {
-  console.log('[PWA Builder] Claiming clients for current page');
-  return self.clients.claim();
-});*/
 self.addEventListener('activate', function(e) {
   console.log('[ServiceWorker] Activate');
   e.waitUntil(
@@ -72,41 +61,3 @@ self.addEventListener('fetch', function(e) {
     );
   }
 });
-
-/*self.addEventListener('fetch', function(evt) {
-  console.log('[PWA Builder] The service worker is serving the asset.'+ evt.request.url);
-  evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
-  evt.waitUntil(update(evt.request));
-});
-
-
-
-function precache() {
-  return caches.open(CACHE).then(function (cache) {
-    return cache.addAll(precacheFiles);
-  });
-}
-
-function fromCache(request) {
-  //we pull files from the cache first thing so we can show them fast
-  return caches.open(CACHE).then(function (cache) {
-    return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
-    });
-  });
-}
-
-function update(request) {
-  //this is where we call the server to get the newest version of the
-  //file to use the next time we show view
-  return caches.open(CACHE).then(function (cache) {
-    return fetch(request).then(function (response) {
-      return cache.put(request, response);
-    });
-  });
-}
-
-function fromServer(request){
-  //this is the fallback if it is not in the cache to go to the server and get it
-  return fetch(request).then(function(response){ return response});
-}*/
