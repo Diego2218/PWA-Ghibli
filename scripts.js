0abe69ea3bfa72a7ef7a9b8e554a69dbb7ba1788
Diego2218 <gogo_18@outlook.com>
2018-08-20 17:01:49 -0500

    fix

--- scripts.js
+++ scripts.js
@@ -10,46 +10,17 @@ app.appendChild(logo);
 app.appendChild(container);
 var url = 'https://ghibliapi.herokuapp.com/films';
 
-
-
-/*request.onload = function () {
-
-  if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
-
-      const card = document.createElement('div');
-      card.setAttribute('class', 'card');
-
-      const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
-
-      const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
-
-      container.appendChild(card);
-      card.appendChild(h1);
-      card.appendChild(p);
-    });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `It's not working! :( `;
-    app.appendChild(errorMessage);
-  }
-
-}*/
-
 function conexion(){
   if(navigator.onLine){
-            var xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function() {
-                if (this.readyState == 4 && this.status == 200) {
-                    cargarDatos(JSON.parse(this.response));
-                }
-            };
-            xhttp.open("GET", url, true);
-            xhttp.send();
-          }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        cargarDatos(JSON.parse(this.response));
+      }
+    };
+    xhttp.open("GET", url, true);
+    xhttp.send();
+  }
   else {
     if ('caches' in window) {
       caches.match(url).then(function(response) {
@@ -58,7 +29,7 @@ function conexion(){
             cargarDatos(json);
           });
         }
-      }
+      });
     }
   }
 }
