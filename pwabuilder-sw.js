f25ed44bc4135f40cad97263936d348e16f5d7f1
Diego2218 <gogo_18@outlook.com>
2018-08-20 16:36:30 -0500

    update

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -52,9 +52,9 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('[Service Worker] Fetch', e.request.url);
   var dataUrl = 'https://ghibliapi.herokuapp.com/films';
   if (e.request.url.indexOf(dataUrl) > -1) {
+    console.log('[Service Worker] Fetch', e.request.url);
     e.respondWith(
       caches.open(dataCacheName).then(function(cache) {
         return fetch(e.request).then(function(response){
